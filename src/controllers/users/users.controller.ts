fe498741635a443aaa4ccd9a2e7c558eba07ed40
J4spr <jasper07.verbruggen@gmail.com>
2023-07-17 16:19:44 +0200

    add post add user function

--- src/controllers/users/users.controller.ts
+++ src/controllers/users/users.controller.ts
@@ -1,5 +1,5 @@
 import { UsersService } from '@/services/users/users.service';
-import { Controller, Get, Header, Param } from '@nestjs/common';
+import { Controller, Get, Header, Param, Post, HttpCode } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
@@ -7,9 +7,35 @@ export class UsersController {
 
   @Get()
   @Header('Content-Type', 'application/json')
-  async getUsers(): Promise<object[]> {
+  async getUsersGet(): Promise<object[]> {
     const users = await this.usersService.getUsers();
     return users;
+  }
+
+  @Post()
+  @HttpCode(201)
+  getUserPost(): object[] {
+    return [
+      {
+        id: '3b68d9d3-e9a2-4c4a-b2d6-6d5a3201f717',
+        email: 'john.doe@smartcampus.com',
+        name: 'John',
+        lastname: 'Doe',
+        password: '?HY#:}{e',
+        'profile-picture': 'https://assets.smartcampus.com/users/Abc12Xyz.jpg',
+        permission: 'admin',
+        'date-of-birth': '2001-03-21',
+        adress: {
+          street: '258 Fairview Blvd',
+          city: 'Hempstead',
+          state: 'New York',
+          zipcode: '11550',
+          country: 'United States',
+        },
+        'creation-time': '1689603030',
+        'update-time': '1689603030',
+      },
+    ];
   }
 
   @Get(':id')
