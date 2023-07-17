f8ef2c1afab915c1a605f6d12a02cc380b9e90a0
Cédric <work@cedricverlinden.com>
2023-07-17 20:05:35 +0200

    Refactored getUsers method to use the Prisma service to retrieve all users. (#5)
    Refactored findUser method to use the Prisma service to find a user by ID.
    
    Add fields lastName, profilePicture, permission, and dateOfBirth to User model.
    
    Add PrismaService class to handle Prisma client initialization.

--- src/controllers/users/users.controller.ts
+++ src/controllers/users/users.controller.ts
@@ -9,6 +9,7 @@ import {
   HttpCode,
   Delete,
 } from '@nestjs/common';
+import { User } from '@prisma/client';
 
 @Controller('users')
 export class UsersController {
@@ -16,40 +17,30 @@ export class UsersController {
 
   @Get()
   @Header('Content-Type', 'application/json')
-  async getUsersGet(): Promise<object[]> {
-    const users = await this.usersService.getUsers();
-    return users;
+  async getUsers(): Promise<User[]> {
+    return await this.usersService.users();
   }
 
   @Post()
   @HttpCode(201)
-  createUser(): object[] {
+  createUser(): User[] {
     return [
       {
         id: '3b68d9d3-e9a2-4c4a-b2d6-6d5a3201f717',
         email: 'john.doe@smartcampus.com',
         name: 'John',
-        lastname: 'Doe',
+        lastName: 'Doe',
         password: '?HY#:}{e',
-        'profile-picture': 'https://assets.smartcampus.com/users/Abc12Xyz.jpg',
+        profilePicture: 'https://assets.smartcampus.com/users/Abc12Xyz.jpg',
         permission: 'admin',
-        'date-of-birth': '2001-03-21',
-        adress: {
-          street: '258 Fairview Blvd',
-          city: 'Hempstead',
-          state: 'New York',
-          zipcode: '11550',
-          country: 'United States',
-        },
-        'creation-time': '1689603030',
-        'update-time': '1689603030',
+        dateOfBirth: new Date(Date.now()),
       },
     ];
   }
 
   @Get(':id')
-  getUser(@Param('id') id: string): { message: string } {
-    return { message: `User ${id} found` };
+  async getUser(@Param('id') id: string): Promise<User | null> {
+    return this.usersService.findUser(id);
   }
 
   @Patch(':id')
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,9 +1,10 @@
 import { UsersService } from './../../services/users/users.service';
 import { UsersController } from '@/controllers/users/users.controller';
+import { PrismaService } from '@/services/prisma/prisma.service';
 import { Module } from '@nestjs/common';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, PrismaService],
 })
 export class UsersModule {}
