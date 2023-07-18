7f5a66d47a5603264dcb32f0b9f8fd0e6c73bf05
Cédric <work@cedricverlinden.com>
2023-07-18 19:56:12 +0200

    Add new testing workflow to the GitHub Actions configuration file
    Add test file for AppController

---
+++ src/controllers/app/app.controller.spec.ts
@@ -0,0 +1,28 @@
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { AppController } from './app.controller';
+import { AppService } from '@/services/app/app.service';
+import * as request from 'supertest';
+
+describe('AppController', () => {
+  let controller: AppController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService],
+    }).compile();
+
+    controller = module.get<AppController>(AppController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  describe('HelloWorld', () => {
+    it('should return a status code of 200', () => {
+      request(controller.HelloWorld()).get('/').expect(200);
+    });
+  });
+});
