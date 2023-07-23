3723857c915699d0bb77fa4d7430ba366776365b
Cédric <work@cedricverlinden.com>
2023-07-24 01:37:06 +0200

    update docker-compose

--- prisma/data/classes.ts
+++ prisma/data/classes.ts
@@ -1,17 +1,16 @@
 import { faker } from '@faker-js/faker';
 import { CreateClassDto } from '@/modules/classes/dtos/create-class.dto';
-import { SchoolClass, schoolClasses } from './schoolClasses';
+import { schoolClasses } from './schoolClasses';
 
 export const classes = (): CreateClassDto[] => {
   let users: CreateClassDto[] = [];
-  for (let i = 0; i < 50; i++) {
-    const aClass: SchoolClass = faker.helpers.arrayElement(schoolClasses);
+  schoolClasses.forEach((aClass) => {
     users.push({
       name: aClass.name,
       teacherId: faker.database.mongodbObjectId(),
       description: aClass.description,
     });
-  }
+  });
 
   return users;
 };
