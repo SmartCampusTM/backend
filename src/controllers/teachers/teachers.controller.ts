2e8279833e06ad3149867c7eab9855e7d2e7763c
Cédric <work@cedricverlinden.com>
2023-07-24 12:55:52 +0200

    feat(teacher): add TeachersController and TeachersService
    Add corresponding tests

---
+++ src/controllers/teachers/teachers.controller.ts
@@ -0,0 +1,51 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  ValidationPipe,
+  Param,
+} from '@nestjs/common';
+
+import { Teacher } from '@prisma/client';
+
+import { CreateTeacherDto } from '@/modules/teachers/dtos/create-teacher.dto';
+import { UpdateTeacherDto } from '@/modules/teachers/dtos/update-teacher.dto';
+import { TeachersService } from '@/services/teachers/teachers.service';
+
+@Controller('teachers')
+export class TeachersController {
+  constructor(private readonly teachersService: TeachersService) {}
+
+  @Post()
+  async create(
+    @Body(new ValidationPipe()) createTeacherDto: CreateTeacherDto,
+  ): Promise<Teacher | null> {
+    return await this.teachersService.createTeacher(createTeacherDto);
+  }
+
+  @Get()
+  async findAll(): Promise<Teacher[] | null> {
+    return await this.teachersService.teachers();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<Teacher | null> {
+    return await this.teachersService.findTeacher(id);
+  }
+
+  @Patch(':id')
+  async update(
+    @Param('id') id: string,
+    @Body(new ValidationPipe()) updateTeacherDto: UpdateTeacherDto,
+  ): Promise<Teacher | null> {
+    return await this.teachersService.updateTeacher(id, updateTeacherDto);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string): Promise<string> {
+    return await this.teachersService.deleteTeacher(id);
+  }
+}
